9f6051f1a7bacd21d6a6614897a214fcce10078a
Zak Johnson <me@zakj.net>
2017-12-01 00:06:21 -0800

    Title -> Bek Stone

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,7 @@ module.exports = {
   ],
 
   head: {
-    title: 'bek.is',
+    title: 'Bek Stone',
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
