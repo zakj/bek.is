c770f5ee74b160e9083840d87ac2cb6c8990836f
Zak Johnson <me@zakj.net>
2017-12-04 20:58:12 -0800

    Fix up margins on desktop

--- components/Container.vue
+++ components/Container.vue
@@ -6,14 +6,13 @@
   @require '~assets/util'
 
   .container
-    box-sizing content-box
     margin 0 auto
-    max-width desktop
+    width 100%
     +below(tablet)
       padding 0 mobile-padding
     +above(tablet)
       padding 0 tablet-padding
-      width 100%
     +above(desktop)
       padding-left tablet-padding + logo-size + tablet-padding
+      max-width desktop + @padding-left + tablet-padding
 </style>
