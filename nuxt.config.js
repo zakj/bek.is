cf399cb3d1803e9d3839ac4f5cd4f0792942f4f0
Zak Johnson <me@zakj.net>
2017-12-27 11:45:38 -0800

    Add html lang=en

--- nuxt.config.js
+++ nuxt.config.js
@@ -79,6 +79,9 @@ module.exports = {
 
   head: {
     title: 'Bek Stone',
+    htmlAttrs: {
+      lang: 'en',
+    },
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
